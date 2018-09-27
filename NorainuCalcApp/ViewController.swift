0e87f5721e3b77cd5aa08a4e3d60bd988a6d4d37
modeverv <modeverv@gmail.com>
2018-09-27 22:02:39 +0900

    Initial Commit

---
+++ NorainuCalcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NorainuCalcApp
+//
+//  Created by norainu on 2018/09/27.
+//  Copyright © 2018 norainu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
