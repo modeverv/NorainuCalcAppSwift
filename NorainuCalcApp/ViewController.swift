bf12f5410068287e8422373d3043b9a929c97633
modeverv <modeverv@gmail.com>
2018-10-01 00:43:35 +0900

    2018-10-01 00:43:35

--- NorainuCalcApp/ViewController.swift
+++ NorainuCalcApp/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var priceField: UITextField!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func tap1Button(_ sender: Any) {
+    addNumber(number: "1")
+  }
+
+  @IBAction func tap00Button(_ sender: Any) {
+    addNumber(number: "00")
+  }
+
+  @IBAction func tapCButton(_ sender: Any) {
+      priceField.text = "0"
+  }
+
+  func addNumber(number: String) {
+    let value = priceField.text! + number
+    if let price = Int(value) {
+      priceField.text = "\(price)"
+    }
   }
 
 
